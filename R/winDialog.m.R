1cd69c555189f54154bbc3f26b9354a40c519c53
Zdealveindy <zeleny.david@gmail.com>
2015-01-27 21:58:49 +0100

    version 3.3.10

--- R/winDialog.m.R
+++ R/winDialog.m.R
@@ -5,7 +5,7 @@ function (type = c("ok", "okcancel", "yesno", "yesnocancel"), message)
 #    if (!interactive()) 
 #        stop("winDialog() cannot be used non-interactively")
     type <- match.arg(type)
-    res <- .External2(winDialog(type, message))
+    res <- .External2(C_winDialog(type, message))
     if (res == 10L) 
         return(invisible(NULL))
     c("NO", "CANCEL", "YES", "OK")[res + 2L]
